7dd19ec4c40d4ccb402d21d5576fa7e6a71bec53
Sergey <stadukhinsergey@gmail.com>
2025-04-14 15:31:28 +0300

    init project

--- api/proxy.ts
+++ api/proxy.ts
@@ -8,11 +8,17 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   const targetUrl = `${baseUrl}${query}`;
 
   try {
+    // Выполнение запроса с добавленными заголовками CORS
     const apiRes = await fetch(targetUrl, {
       method,
-      headers: { 'Content-Type': 'application/json' },
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',  // Разрешение всех доменов
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE', // Разрешенные HTTP методы
+      },
     });
 
+    // Обработка ответа от целевого API
     const data = await apiRes.json();
     res.status(apiRes.status).json(data);
   } catch (error) {
